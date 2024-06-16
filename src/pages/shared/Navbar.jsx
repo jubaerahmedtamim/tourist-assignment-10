c92d62a4518dc098d328406929bb20223a6f9ad0
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-17 02:42:32 +0600

    logo added

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                         {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <Link to='/' className="btn btn-ghost text-2xl font-bold">Green Excursion</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
