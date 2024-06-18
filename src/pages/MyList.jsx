52ec37c89afbb25166b8aa1dd7af1dc782a89548
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-18 15:05:58 +0600

    tourist spot deleted from my list

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CiEdit } from 'react-icons/ci';
 import { GrView } from 'react-icons/gr';
 import { MdDeleteOutline } from 'react-icons/md';
 import { Link, useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const MyList = () => {
     const loadedTouristSpots = useLoaderData();
+    const [touristSpots, setTouristSpots] = useState(loadedTouristSpots);
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/spot/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your data has been deleted.",
+                                icon: "success"
+                            });
+                            const remainingSpots =  touristSpots.filter(spot => spot._id !== id)
+                            setTouristSpots(remainingSpots)
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div className="overflow-x-auto w-5/6 mx-auto mb-10">
             <h1 className=' font-bold text-4xl text-center my-10'>My <span className='text-[#52b788]'>Tourist Spot</span> List</h1>
@@ -23,7 +57,7 @@ const MyList = () => {
                 </thead>
                 <tbody>
                     {
-                        loadedTouristSpots.map((spot, index) => <tr className='hover:bg-green-50' key={spot._id}>
+                        touristSpots.map((spot, index) => <tr className='hover:bg-green-50' key={spot._id}>
                             <th>{index + 1}</th>
                             <td>{spot?.userName}</td>
                             <td>{spot?.spot_name}</td>
